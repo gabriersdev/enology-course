f53a24d14cd5c0a4db1a0077ae726ef9f721a775
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2024-12-17 13:55:18 -0300

    feat: meta props, favicon e imgs

--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -15,7 +15,6 @@ export default function DefaultLayout({
   useEffect(() => {
     AOS.init({
       once: true,
-      disable: "phone",
       duration: 700,
       easing: "ease-out-cubic",
     });
